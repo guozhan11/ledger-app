dee53e5d72e36314b91309e361e9e4b9d6e99f6c
guozhan11 <120020012@link.cuhk.edu.cn>
2026-08-17 13:15:06 -0400

    Add non-calculating income records

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ledger-shell-v8';
+const CACHE_NAME='ledger-shell-v9';
 const BASE_URL=new URL('./',self.location.href);
 const INDEX_URL=new URL('index.html',BASE_URL).href;
 const APP_SHELL=[
